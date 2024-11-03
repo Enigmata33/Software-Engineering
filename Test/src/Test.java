149b1afa2cffa27c3a3721922b9499715bc79302
Enigmata33 <2253702260@qq.com>
2024-11-03 12:26:52 +0000

    Test

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,6 +2,10 @@ public class Test {
     public static void main(String[] args) {
         int x = 1;
         System.out.println(x);
+        System.out.println("hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
 
+        }
     }
 }
